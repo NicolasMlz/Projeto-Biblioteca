e6147e4d7d3b84049c84ac8723c509722e3e8b48
Nicolas Maulaiz <nnicolasmaulaiz@gmail.com>
2022-08-30 20:37:32 -0300

    30-08-2022
    5

--- teste/backendTeste/BibliotecaTeste.java
+++ teste/backendTeste/BibliotecaTeste.java
@@ -21,32 +21,11 @@ class BibliotecaTeste {
         boolean removerCliente = Biblioteca.removerCliente(999999999L);
         assertFalse(removerCliente);
     }
-	
-	@Test
-    void testeRemoverClienteVerdadeiro() {
-		
-		Arquivo.ReadCliente("cliente.txt");
-		
-        boolean removerCliente = Biblioteca.removerCliente(22222222222L);
-        assertTrue(removerCliente);
-    }
     
     @Test
-    void testeAdicionarBibliotecarioFalso() {
-    	
-		Arquivo.ReadBibliotecario("bibliotecario.txt");
-
-    	Bibliotecario b = new Bibliotecario(43688302810L, "Nicolas Marcelo Maulaiz", "11931525036", 2L, "nic", "321");
-        boolean adicionar = Biblioteca.adicionarBibliotecario(b);
-        assertFalse(adicionar);
+    void testeRemoverBibliotecarioFalso() {
+        boolean removerBibliotecario = Biblioteca.removerBibliotecario(-988928L);
+        assertFalse(removerBibliotecario);
     }
     
-    @Test
-	void testeRemoverLivroVerdadeiro() {
-    	
-		Arquivo.ReadLivro("livro.txt");
-
-        boolean removerLivro = Biblioteca.removerLivroTotal(1L);
-        assertTrue(removerLivro);
-    }
 } 
